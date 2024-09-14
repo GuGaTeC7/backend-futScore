2e620ca990ec2fd7a72a823658a0f90c3a4f8690
GuGaTeC7 <gustavoluizsilva245@gmail.com>
2024-09-14 16:27:14 -0300

    Iniciando back do FutScore

---
+++ src/main/java/com/backend/futScore/FutScoreApplication.java
@@ -0,0 +1,13 @@
+package com.backend.futScore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutScoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/futScore/FutScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.futScore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
